e64195e428a57ed89c3a5fcc0dcc6d0004ea3743
Roberto <robetatis@yahoo.com>
2020-06-26 17:36:29 +0200

    .

--- class.py
+++ class.py
@@ -3,6 +3,19 @@ import random
 from sklearn.linear_model import Perceptron
 from sklearn.neighbors import KNeighborsClassifier
 
+# perceptron learning rule: w = w + alpha(y - hw(x))*x,
+# with hw(x) = w0 + w1*x2 + w2*x2 >= 0 -> y = 1
+#                                 < 0  -> y = 0
+
+
+# use atmospheric pressure and temperature to predict whether it rains or not
+
+# use MySQM to put hourly data from dwd climate data center into database
+# connect and grab data
+# classify with linear perceptron
+# classify with k-neighbors
+
+
 model = Perceptron()
 model = KNeighborsClassifier(n_neighbors=1)
 
@@ -16,6 +29,7 @@ with open("banknotes.csv") as f:
             "evidence": [float(cell) for cell in row[:4]],
             "label": "Authentic" if row[4] == "0" else "Counterfeit"
         })
+
 
 # Separate data into training and testing groups
 holdout = int(0.40 * len(data))
