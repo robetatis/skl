1bb38de1a90f717aa1cd7dafd02a85b307989e35
Roberto <robetatis@yahoo.com>
2020-06-29 20:47:06 +0200

    v1

--- class.py
+++ class.py
@@ -1,3 +1,4 @@
+# load packages
 import csv
 import random
 import numpy as np
@@ -5,7 +6,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import Perceptron
 from sklearn.neighbors import KNeighborsClassifier
-
 
 # import data
 dateparser = lambda x: pd.to_datetime(x, format='%Y%m%d%H')
@@ -16,27 +16,40 @@ temp = pd.read_csv('temp.txt', delimiter=';', usecols=[1, 4, 5], names=['dateTim
 rain = pd.read_csv('rain.txt', delimiter=';', usecols=[1, 4], names=['dateTime', 'rain'], 
                     skiprows=1, parse_dates=['dateTime'], date_parser=dateparser, index_col='dateTime')
 
+# combine into single dataframe
 df = rain
 df['atmpress'] = press['pressure']
 df['airtemp'] = temp['temperature']
 df['relhum'] = temp['relative_humidity']
 
-# clean data
+# remove no data (labeled as -999)
 df = df[df != -999]
-
-
-plt.plot(df)
-plt.show()
-
+df = df.dropna()
 
 # plot hourly variables
-
+plt.close()
+plt.figure()
+plt.subplots_adjust(top=0.9, bottom=0.9)
+plt.subplot(311)
+plt.plot(df['rain'], 'b.', markersize=3)
+plt.ylabel('Rain [mm/hour]')
+plt.yticks(np.linspace(0, 40, 5))
+plt.subplot(312)
+plt.plot(df['airtemp'], '-r', linewidth=0.3)
+plt.yticks(np.linspace(-10, 40, 6))
+plt.ylabel('Temp. [ºC]')
+plt.subplot(313)
+plt.plot(df['atmpress'], '-g', linewidth=0.3)
+plt.ylabel('Atm. Press [hPa]')
+plt.yticks(np.linspace(980, 1040, 5))
+plt.suptitle('Hourly data')
+plt.show()
 
 # aggregate to daily values using different statistics (max, median, mean)
-
-
-
-# build classification model
+df['year'] = df.index.year
+df['month'] = df.index.month
+df['day'] = df.index.day
+# ...
 
 # perceptron learning rule: w = w + alpha(y - hw(x))*x,
 # with hw(x) = w0 + w1*x2 + w2*x2 >= 0 -> y = 1
@@ -46,47 +59,43 @@ plt.show()
 model = Perceptron()
 #model = KNeighborsClassifier(n_neighbors=1)
 
-with open("banknotes.csv") as f:
-    reader = csv.reader(f)
-    next(reader)
-
-    data = []
-    for row in reader:
-        data.append({
-            "evidence": [float(cell) for cell in row[:4]],
-            "label": "Authentic" if row[4] == "0" else "Counterfeit"
+data = []
+for index, row in df.iterrows():
+    data.append({
+        "evidence": [float(ij) for ij in row[1:4]],
+        "label": 1 if row[0] > 0 else 0
         })
 
 
-# Separate data into training and testing groups
+# split training and testing sets
 holdout = int(0.40 * len(data))
 random.shuffle(data)
 testing = data[:holdout]
 training = data[holdout:]
 
-# Train model on training set
+# train model
 X_training = [row["evidence"] for row in training]
 y_training = [row["label"] for row in training]
 model.fit(X_training, y_training)
 
-# Make predictions on the testing set
+# predict on testing set
 X_testing = [row["evidence"] for row in testing]
 y_testing = [row["label"] for row in testing]
 predictions = model.predict(X_testing)
 
-# Compute how well we performed
-correct = 0
-incorrect = 0
-total = 0
+# prediction quality
+corr = 0
+incorr = 0
+tot = 0
 for actual, predicted in zip(y_testing, predictions):
-    total += 1
+    tot += 1
     if actual == predicted:
-        correct += 1
+        corr += 1
     else:
-        incorrect += 1
+        incorr += 1
 
 # Print results
 print(f"Results for model {type(model).__name__}")
-print(f"Correct: {correct}")
-print(f"Incorrect: {incorrect}")
-print(f"Accuracy: {100 * correct / total:.2f}%")
+print(f"Correct: {corr}")
+print(f"Incorrect: {incorr}")
+print(f"Accuracy: {100 * corr / tot:.2f}%")
